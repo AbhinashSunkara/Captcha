e9ddc462f45113be30890475a1c13638e1f45569
AbhinashSunkara <114418667+AbhinashSunkara@users.noreply.github.com>
2022-10-11 19:05:02 +0530

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -2,7 +2,7 @@ import random
 from tkinter import*
 root=Tk()
 root.title("CATPTCHA check")
-root.geometry("500x500")
+root.geometry("800x800")
 p="1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*?ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 global k
 global b
@@ -12,11 +12,12 @@ key=StringVar()
 k="".join(random.sample(p,4))
 key.set(k)
 bf=Frame(root).grid()
-Label(root,text="Captcha :",bg="#2C3E50", fg="#EAECEE", font="verdana 10 bold").grid(column=4,row=1)
+l1=Label(root,text="Captcha :",bg="#2C3E50", fg="#EAECEE", font="verdana 10 bold")
+l1.place(relx=0.4,rely=0.3,anchor=CENTER)
 e5=Entry(root,textvariable=key)
-e5.grid(column=4,row=2)
+e5.place(relx=0.4,rely=0.35,anchor=CENTER)
 l2= Label(root,text="ENTER Captcha" ,bg="#2C3E50", fg="#EAECEE", font="verdana 10 bold")
-l2.grid(column=4,row=3)
+l2.place(relx=0.4,rely=0.4,anchor=CENTER)
 global a
 a=0
 def click():
@@ -26,19 +27,19 @@ def click():
         global b
         b=1
         l4=Label(root,text="continue",bg="#98FB98", fg="#033", font="verdana 12 bold")
-        l4.grid(column=8,row=3)
+        l4.place(relx=0.7,rely=0.3,anchor=CENTER)
     else:
         global l5
         global a
         a=1
         l5=Label(root,text="Give the correct captche",bg="#98FB98", fg="#033", font="verdana 12 bold")
-        l5.grid(column=8,row=6)
+        l5.place(relx=0.7,rely=0.3,anchor=CENTER)
 def cl():
     global k
     k="".join(random.sample(p,4))
     key.set(k)
     e5=Entry(root,textvariable=key)
-    e5.grid(column=4,row=2)
+    e5.place(relx=0.4,rely=0.35,anchor=CENTER)
     global l3
 def cli():
     if (b==1):
@@ -48,10 +49,10 @@ def cli():
     e2.delete(0,END)
 var=StringVar()
 e2= Entry(root,textvariable=var,width=20)
-e2.grid(column=4,row=6)
+e2.place(relx=0.4,rely=0.45,anchor=CENTER)
 
-Button(root,text="Submit",bg="#fdde6c", fg="#000", font="verdana 12 bold",command=click).grid(column=3,row=7)
-Button(root,text="Refresh",bg="#fdde6c", fg="#000", font="verdana 12 bold",command=cl).grid(column=4,row=7)
-Button(root,text="Clear",bg="#fdde6c", fg="#000", font="verdana 12 bold",command=cli).grid(column=7,row=7)
+Button(root,text="Submit",bg="#fdde6c", fg="#000", font="verdana 12 bold",command=click).place(relx=0.3,rely=0.5,anchor=CENTER)
+Button(root,text="Refresh",bg="#fdde6c", fg="#000", font="verdana 12 bold",command=cl).place(relx=0.43,rely=0.5,anchor=CENTER)
+Button(root,text="Clear",bg="#fdde6c", fg="#000", font="verdana 12 bold",command=cli).place(relx=0.54,rely=0.5,anchor=CENTER)
 
 root.mainloop()
